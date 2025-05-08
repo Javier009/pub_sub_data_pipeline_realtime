b0e8c8dc8127381da2c82038cb33f5367a298784
Javier009 <38794360+Javier009@users.noreply.github.com>
2025-05-08 16:49:35 -0700

    first commit

---
+++ producer.py
@@ -0,0 +1,87 @@
+# pip install avro-python3
+# pip install google-cloud-pubsub
+
+import io
+import time
+import json
+import uuid
+import random
+import datetime
+import string
+import avro.schema
+from avro.io import DatumWriter, BinaryEncoder
+from google.pubsub_v1.types.schema import Schema, Encoding, GetSchemaRequest, SchemaView
+from google.pubsub_v1.services.publisher import PublisherClient
+from google.pubsub_v1.services.schema_service import SchemaServiceClient
+from google.pubsub_v1.types.pubsub import PubsubMessage, PublishRequest
+
+PROJECT_ID  = "real-time-data-pipeline-457520"
+TOPIC_ID = "orders-topic"
+
+def fetch_topic_schema_and_encoding(project_id: str, topic_id: str):
+    publisher    = PublisherClient()
+    topic_path   = publisher.topic_path(project_id, topic_id)
+    topic        = publisher.get_topic(request={"topic": topic_path})
+    schema_name  = topic.schema_settings.schema
+    encoding     = topic.schema_settings.encoding
+    schema_client = SchemaServiceClient()
+    req = GetSchemaRequest(
+        name=schema_name,
+        view=SchemaView.FULL
+    )
+    schema_obj   = schema_client.get_schema(request=req)
+    avro_schema  = avro.schema.parse(schema_obj.definition)
+    return avro_schema, encoding, topic_path
+
+def mock_order() -> dict:
+    today = datetime.date.today()
+    return {
+      "order_id":  str(uuid.uuid4()),
+      "location":  f"user-{random.randint(1000,9999)}",
+      "user_id":   f"{''.join([random.choice(string.ascii_letters.lower()) for i in range(5)])}-{random.randint(100,199)}",
+      "item_id":   f"{''.join([random.choice(string.ascii_letters.lower()) for i in range(5)])}-{random.randint(100,199)}",
+      "item_category": random.choice(["electronics", "home", 'garden', "kitchen", "clothes", "toys", "videogames"]),
+      "purchase_date":today.isoformat(),
+      "price": random.randint(1000,9999),
+      "currency": "USD",
+      "discount": random.randint(0,20)/100,
+      "status":   random.choice(["CONFIRMED","CANCELLED","PENDING", "SENT","DELIVERED"])
+    }
+
+def serialize_record(avro_schema, record: dict) -> bytes:
+    buf     = io.BytesIO()
+    encoder = BinaryEncoder(buf)
+    writer  = DatumWriter(avro_schema)
+    writer.write(record, encoder)
+    return buf.getvalue()
+
+def main():
+    avro_schema, encoding, topic_path = fetch_topic_schema_and_encoding(PROJECT_ID, TOPIC_ID)     
+    publisher = PublisherClient()     
+    # A list to simulate wrong records entering with not the proper format
+    #  right_data_list = [True, False, True, True, True, True, True, False, True, True, True, False]
+    for n in range(10):
+        rec = mock_order()
+        # random_data_quality_choise  = random.choice(right_data_list)
+        if encoding == Encoding.BINARY:
+            try:
+                data = serialize_record(avro_schema, rec)
+            except ValueError:
+                raise('Data does not respect the AVRON schema, wont pass to TOPIC')
+
+        elif encoding == Encoding.JSON:
+            data = json.dumps(rec).encode('utf-8')            
+        else:
+            raise RuntimeError('Unsupported input data formats')
+        # Send Message to Pub/Sub Topic
+        msg = PubsubMessage(data=data)
+        req = PublishRequest(topic=topic_path, messages=[msg])
+        resp = publisher.publish(request=req)    
+        print(f"Published order_id={rec['order_id']} as msg_id={resp.message_ids[0]}")
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
+
+
+
